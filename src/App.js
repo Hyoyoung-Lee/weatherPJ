48cc38a953d96d4b17fa42db437f03bf70c290b3
Hyoyoung Lee <lhy2361@naver.com>
2022-06-17 14:52:58 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+// 클래스형으로 만들어보기!
+class App extends React.Component {
+    state = {
+        isLoading: true,
+        stateWeather: []
+    };
+
+    getWeather = async () => {
+        // const weathers : axios 해서 온 data를 잡아야 state에 사용할 수 있음
+        const axiosWeather = await axios.get("https://api.weatherapi.com/v1/forecast.json?key=dfe315ea852049d29eb63038221606&q=Seoul&days=6");
+        this.setState({ stateWeather : axiosWeather.data, isLoading : false });
+        console.log(axiosWeather.data);
+    };
+
+    componentDidMount() {
+        this.getWeather();
+    }
+
+    render() {
+        const { isLoading } = this.state;
+        return (
+            <div>
+                {isLoading ? "Loading..." : "We are ready"}
+
+            </div>
+          );
+    }
 }
 
 export default App;
---
+++ src/Weather.js
@@ -0,0 +1,6 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class Weather extends React.Component {
+
+}
